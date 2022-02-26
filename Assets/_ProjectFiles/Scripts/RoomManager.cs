820f3d4d804b27ace10546a13f0999cba30d90f5
TheJayDuck <ardafevzi.armutcu@gmail.com>
2022-02-27 01:09:44 +0300

    Some Changes

--- Assets/_ProjectFiles/Scripts/RoomManager.cs
+++ Assets/_ProjectFiles/Scripts/RoomManager.cs
@@ -4,10 +4,12 @@ public class RoomManager : MonoBehaviour
 {
     public int RoomIndex;
 
+    public Room[] Rooms;
 }
 
 [System.Serializable]
 public class Room {
+    public string Name = "New Room";
     public Sprite Sprite;
     public AudioClip Clip;
 } 
