cb7c0a5af9c0e5215fcd2cfad8b5aad333e452bf
Nobbele <realnobbele@gmail.com>
2022-03-20 17:57:26 +0100

    Adds save and load

--- Assets/_ProjectFiles/Scripts/SaveManager.cs
+++ Assets/_ProjectFiles/Scripts/SaveManager.cs
@@ -1,8 +1,14 @@
-using UnityEngine;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using Newtonsoft.Json;
+using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
+    private static string SAVE_LOC => Application.dataPath + "/Save/main.sav";
+
     private PetStats stats;
     private WardrobeManager wardrobe;
     private RoomManager room;
@@ -12,68 +18,127 @@ public class SaveManager : MonoBehaviour
         stats = PetStats.Instance;
         wardrobe = WardrobeManager.Instance;
         room = RoomManager.Instance;
-
-        SaveSystem.Initialize();
     }
-    
+
     private void OnGUI()
     {
-         if (GUI.Button(new Rect(10, 10, 50, 50), "Save"))
-         {
-            Debug.Log("Saving");
+        if (GUI.Button(new Rect(10, 10, 50, 50), "Save"))
+        {
+            Debug.Log("Saving..");
             Save();
-         }
-         if (GUI.Button(new Rect(10, 100, 50, 50), "Load"))
-         {
-            Debug.Log("Loading");
+        }
+        if (GUI.Button(new Rect(10, 100, 50, 50), "Load"))
+        {
+            Debug.Log("Loading..");
             Load();
-         } 
+        }
+    }
+
+    struct SaveFile
+    {
+        public string IV;
+        public string Data;
     }
 
     void Save()
     {
-        float _experience = stats.TotalExperience;
-        int _level = stats.CurrentLevel;
+        SaveObject saveObject = new SaveObject
+        {
+            Experience = stats.TotalExperience,
+            Level = stats.CurrentLevel,
 
-        float _affection = stats.Affection.Value;
-        float _hunger = stats.Hunger.Value;
-        float _cleanliness = stats.Cleanliness.Value;
+            Affection = stats.Affection.Value,
+            Hunger = stats.Hunger.Value,
+            Cleanliness = stats.Cleanliness.Value,
 
-        int _hatIndex = wardrobe.Hats.Index;
-        int _glassesIndex = wardrobe.Glasses.Index;
-        int _dressIndex = wardrobe.Dress.Index;
-        int _accessoryIndex = wardrobe.Accessories.Index;
+            HatIndex = wardrobe.Hats.Index,
+            GlassesIndex = wardrobe.Glasses.Index,
+            DressIndex = wardrobe.Dress.Index,
+            AccessoryIndex = wardrobe.Accessories.Index,
 
-        int _roomIndex = room.CurrentRoom;
-
-        SaveObject saveObject = new SaveObject {
-            Experience = _experience,
-            Level = _level,
-
-            Affection = _affection,
-            Hunger = _hunger,
-            Cleanliness = _cleanliness,
-
-            HatIndex = _hatIndex,
-            GlassesIndex = _glassesIndex,
-            DressIndex = _dressIndex,
-            AccessoryIndex = _accessoryIndex,
-
-            RoomIndex = _roomIndex,
+            RoomIndex = room.CurrentRoom,
         };
+
+        if (!Directory.Exists(SAVE_LOC))
+        {
+            Directory.CreateDirectory(Directory.GetParent(SAVE_LOC).ToString());
+        }
+
         string json = JsonConvert.SerializeObject(saveObject);
-        SaveSystem.Save(json);
+
+        string iv;
+        string data;
+
+        using (var aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String("UJ3JMqwz+uD/nIVQDbDhtLHj39E77Am6X3yd9pRKjFQ=");
+            aes.GenerateIV();
+            iv = Convert.ToBase64String(aes.IV);
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using var ms = new MemoryStream();
+            using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+            using (var sw = new StreamWriter(cs))
+            {
+                sw.Write(json);
+            }
+            data = Convert.ToBase64String(ms.ToArray());
+        }
+
+        using (var fs = File.OpenWrite(SAVE_LOC))
+        using (var sw = new StreamWriter(fs))
+        using (var js = new JsonTextWriter(sw))
+        {
+            var ser = new JsonSerializer();
+            ser.Serialize(js, new SaveFile()
+            {
+                IV = iv,
+                Data = data
+            });
+        }
     }
 
     void Load()
     {
-        string result = SaveSystem.Load();
-        Debug.Log(result);
-
-        if(result != null) {
-            SaveObject saveObject = JsonConvert.DeserializeObject<SaveObject>(result);
-            // stats.Initialize(saveObject);
+        SaveFile save;
+        using (var fs = File.OpenRead(SAVE_LOC))
+        using (var sw = new StreamReader(fs))
+        using (var js = new JsonTextReader(sw))
+        {
+            var ser = new JsonSerializer();
+            save = ser.Deserialize<SaveFile>(js);
         }
+
+        var data = Convert.FromBase64String(save.Data);
+        string json;
+
+        using (var aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String("UJ3JMqwz+uD/nIVQDbDhtLHj39E77Am6X3yd9pRKjFQ=");
+            aes.IV = Convert.FromBase64String(save.IV);
+            var encryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using var ms = new MemoryStream(data);
+            using CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Read);
+            using var sw = new StreamReader(cs);
+            json = sw.ReadToEnd();
+        }
+
+        var saveData = JsonConvert.DeserializeObject<SaveObject>(json);
+
+        stats.TotalExperience = saveData.Experience;
+        stats.CurrentLevel = saveData.Level;
+
+        stats.Affection.Value = saveData.Affection;
+        stats.Hunger.Value = saveData.Hunger;
+        stats.Cleanliness.Value = saveData.Cleanliness;
+
+        wardrobe.Set(wardrobe.Hats, saveData.HatIndex);
+        wardrobe.Set(wardrobe.Glasses, saveData.GlassesIndex);
+        wardrobe.Set(wardrobe.Dress, saveData.DressIndex);
+        wardrobe.Set(wardrobe.Accessories, saveData.AccessoryIndex);
+
+        room.SwitchRoom(saveData.RoomIndex);
     }
 }
 
@@ -82,7 +147,7 @@ public struct SaveObject
     // Progress
     public float Experience;
     public int Level;
-    
+
     // Stats
     public float Affection;
     public float Hunger;
--- Assets/_ProjectFiles/Scripts/WardrobeManager.cs
+++ Assets/_ProjectFiles/Scripts/WardrobeManager.cs
@@ -7,49 +7,51 @@ public class WardrobeManager : SingletonBehaviour<WardrobeManager>
     public Wardrobe Dress;
     public Wardrobe Accessories;
 
-   public void Initialize(SaveObject save) // TODO load data from save
-   {
-       Hats.Index = save.HatIndex;
-       Glasses.Index = save.GlassesIndex;
-       Dress.Index = save.DressIndex;
-       Accessories.Index = save.AccessoryIndex;
-   }
+    public void Initialize(SaveObject save) // TODO load data from save
+    {
+        Hats.Index = save.HatIndex;
+        Glasses.Index = save.GlassesIndex;
+        Dress.Index = save.DressIndex;
+        Accessories.Index = save.AccessoryIndex;
+    }
 
-    public void Next(int target) {
+    public void Next(int target)
+    {
         var wardrobe = GetWardrobe(target);
-        wardrobe.Index = Repeat(wardrobe.Index + 1, wardrobe.Options.Length);
+        Set(wardrobe, wardrobe.Index + 1);
+    }
+
+    public void Prev(int target)
+    {
+        var wardrobe = GetWardrobe(target);
+        Set(wardrobe, wardrobe.Index - 1);
+    }
+
+    public void Set(Wardrobe wardrobe, int value)
+    {
+        wardrobe.Index = Repeat(value, wardrobe.Options.Length);
         wardrobe.Target.sprite = wardrobe.Options[wardrobe.Index];
     }
 
-    public void Prev(int target) {
-        var wardrobe = GetWardrobe(target);
-        wardrobe.Index = Repeat(wardrobe.Index - 1, wardrobe.Options.Length);
-        wardrobe.Target.sprite = wardrobe.Options[wardrobe.Index];
-    }
-
-    public Wardrobe GetWardrobe(int target) {
-        switch (target) {
-            case 0:
-                return Hats;
-            case 1:
-                return Glasses;
-            case 2:
-                return Dress;
-            case 3:
-                return Accessories;
-            default:
-                throw new System.ArgumentException();
-        }
-    }
+    public Wardrobe GetWardrobe(int target)
+    => target switch
+    {
+        0 => Hats,
+        1 => Glasses,
+        2 => Dress,
+        3 => Accessories,
+        _ => throw new System.ArgumentException(),
+    };
 
     int Repeat(int v, int max) => (v % max + max) % max;
 }
 
 [System.Serializable]
-public class Wardrobe {
+public class Wardrobe
+{
 
     public int Index;
 
     public SpriteRenderer Target;
     public Sprite[] Options;
-} 
+}
